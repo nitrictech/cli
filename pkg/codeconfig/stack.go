65cd682802b51886f20c0095ae77a32cff12741c
Rak <rak.siva@nitric.io>
2022-01-10 08:35:15 -0700

    chore: correct licences

--- pkg/codeconfig/stack.go
+++ pkg/codeconfig/stack.go
@@ -1,10 +1,12 @@
-// Copyright 2021 Nitric Pty Ltd.
+// Copyright Nitric Pty Ltd.
+//
+// SPDX-License-Identifier: Apache-2.0
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
+// You may obtain a copy of the License at:
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -20,6 +22,7 @@ import (
 	"sync"
 
 	"github.com/getkin/kin-openapi/openapi3"
+
 	v1 "github.com/nitrictech/apis/go/nitric/v1"
 )
 
@@ -55,7 +58,6 @@ func (s *Stack) GetApiSpec(api string) (*openapi3.T, error) {
 
 	// Collect all workers
 	for _, f := range s.functions {
-
 		workers = append(workers, f.apis[api].workers...)
 	}
 

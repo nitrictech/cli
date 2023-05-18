c2cf0710ee6b1585ae7ec0be09cdc74b3855a1c9
David Moore <davemooreuws@gmail.com>
2023-05-18 16:57:25 +1000

    make path param key more obvious

--- dashboard/src/components/shared/FieldRows.tsx
+++ dashboard/src/components/shared/FieldRows.tsx
@@ -64,7 +64,9 @@ const FieldRows: React.FC<Props> = ({ testId, rows, lockKeys, setRows }) => {
                   value={r.key}
                   name={keyId}
                   id={keyId}
-                  className="block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6"
+                  className={
+                    "block w-full px-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-blue-600 sm:text-sm sm:leading-6 read-only:cursor-not-allowed read-only:bg-gray-50 read-only:ring-gray-200"
+                  }
                 />
               </div>
             </div>

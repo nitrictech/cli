0314f095a251b23cc90c69224818f67400128e18
David Moore <davemooreuws@gmail.com>
2024-01-30 09:04:29 +1100

    clean slashes in service title node

--- pkgplus/dashboard/frontend/src/components/visualizer/nodes/ServiceNode.tsx
+++ pkgplus/dashboard/frontend/src/components/visualizer/nodes/ServiceNode.tsx
@@ -9,12 +9,15 @@ export const ServiceNode: ComponentType<NodeProps<ServiceNodeData>> = ({
   data,
   selected,
 }) => {
+  const cleanedTitle = data.title.replace(/\\/g, "/");
+
   return (
     <NodeBase
       {...data}
+      title={cleanedTitle}
       selected={selected}
       drawerOptions={{
-        title: `Details - ${data.title}`,
+        title: `Details - ${cleanedTitle}`,
         description: data.description,
         children: <div className="flex flex-col">TODO</div>,
       }}

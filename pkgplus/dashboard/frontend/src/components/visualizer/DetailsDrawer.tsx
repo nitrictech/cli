e0fe8b8bdff934bd694a2be79dd0b73585dce258
David Moore <davemooreuws@gmail.com>
2024-01-29 14:58:11 +1100

    adjust node styling

--- pkgplus/dashboard/frontend/src/components/visualizer/DetailsDrawer.tsx
+++ pkgplus/dashboard/frontend/src/components/visualizer/DetailsDrawer.tsx
@@ -8,30 +8,40 @@ import {
   DrawerTitle,
 } from "../ui/drawer";
 import { Button } from "../ui/button";
-import { useState, type PropsWithChildren } from "react";
-import ArrowLongRightIcon from "@heroicons/react/24/outline/ArrowLongRightIcon";
-import { cn } from "@/lib/utils";
+import type { PropsWithChildren } from "react";
+import { useStoreApi } from "reactflow";
 interface Props extends PropsWithChildren {
   title: string;
-  description: string;
+  description?: string;
+  open: boolean;
 }
 
-export const DetailsDrawer = ({ title, description, children }: Props) => {
-  const [open, setOpen] = useState(false);
+export const DetailsDrawer = ({
+  title,
+  description,
+  children,
+  open,
+}: Props) => {
+  const store = useStoreApi();
 
   return (
-    <Drawer direction="right" open={open} onOpenChange={setOpen}>
-      <button className="size-full" onClick={() => setOpen(true)}>
-        <ArrowLongRightIcon
-          className={cn("transition-all", open ? "-rotate-180" : "rotate-0")}
-        />
-      </button>
-
+    <Drawer
+      direction="right"
+      open={open}
+      onOpenChange={(open) => {
+        if (!open) {
+          const { unselectNodesAndEdges } = store.getState();
+          unselectNodesAndEdges();
+        }
+      }}
+    >
       <DrawerContent className="bg-white flex flex-col inset-auto rounded-l-[10px] rounded-r-none h-full w-[400px] mt-24 fixed bottom-0 right-0">
         <div className="mx-auto w-full max-w-sm p-4">
           <DrawerHeader>
             <DrawerTitle>{title}</DrawerTitle>
-            <DrawerDescription>{description}</DrawerDescription>
+            {description && (
+              <DrawerDescription>{description}</DrawerDescription>
+            )}
           </DrawerHeader>
           <div className="p-4">{children}</div>
           <DrawerFooter>

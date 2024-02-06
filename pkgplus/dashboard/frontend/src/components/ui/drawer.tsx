af3523b9579a4206ca12bac556b0bae3778a4557
David Moore <davemooreuws@gmail.com>
2024-02-06 15:03:27 +1100

    make drawer overlay lighter

--- pkgplus/dashboard/frontend/src/components/ui/drawer.tsx
+++ pkgplus/dashboard/frontend/src/components/ui/drawer.tsx
@@ -26,7 +26,7 @@ const DrawerOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DrawerPrimitive.Overlay
     ref={ref}
-    className={cn('fixed inset-0 z-50 bg-black/60', className)}
+    className={cn('fixed inset-0 z-50 bg-black/20', className)}
     {...props}
   />
 ))

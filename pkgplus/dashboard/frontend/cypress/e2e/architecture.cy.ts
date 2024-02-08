be2ec7c5000a015f9ff823f023baa953eb98dd99
David Moore <davemooreuws@gmail.com>
2024-02-08 17:14:11 +1100

    add arch test

---
+++ pkgplus/dashboard/frontend/cypress/e2e/architecture.cy.ts
@@ -0,0 +1,30 @@
+const expectedNodes = [
+  'first-api',
+  'second-api',
+  'socket',
+  'socket-2',
+  'socket-3',
+  'process-tests',
+  'process-tests-2',
+  'test-collection',
+  'connections',
+  'test-bucket',
+  'subscribe-tests',
+  'subscribe-tests-2',
+  'functions/my-test-function.ts',
+]
+
+describe('Architecture Spec', () => {
+  beforeEach(() => {
+    cy.viewport('macbook-16')
+    cy.visit('/visualizer')
+  })
+
+  it('should retrieve correct schedules', () => {
+    cy.wait(500)
+
+    expectedNodes.forEach((content) => {
+      expect(cy.contains('.react-flow__node', content)).to.exist
+    })
+  })
+})

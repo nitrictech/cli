20c1999b08c303251665c70aaf334b132c56354a
Tim Holm <tim.holm@nitric.io>
2024-04-10 09:55:39 +1000

    remove debug statement.

--- pkg/cloud/storage/storage.go
+++ pkg/cloud/storage/storage.go
@@ -343,8 +343,6 @@ type StorageOptions struct {
 
 func corsMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("CORS Middleware")
-
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "GET, OPTIONS, PUT")
 		w.Header().Set("Access-Control-Allow-Headers", "*")

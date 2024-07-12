d9b5b496ea852a4647fcca493f2678791e948178
Jye Cusch <jye.cusch@nitric.io>
2024-07-12 11:57:51 +1000

    lint

--- pkg/dashboard/frontend/src/components/apis/APIExplorer.tsx
+++ pkg/dashboard/frontend/src/components/apis/APIExplorer.tsx
@@ -461,9 +461,7 @@ const APIExplorer = () => {
                             <button
                               type="button"
                               onClick={() => {
-                                copyToClipboard(
-                                  `${apiAddress}${request.path}`,
-                                )
+                                copyToClipboard(`${apiAddress}${request.path}`)
                                 toast.success('Copied Route URL')
                               }}
                             >

d8c22cd572aee6977b12bd4447da54a126a6ba6a
Tim Holm <tim.holm@nitric.io>
2024-07-25 11:16:40 +1000

    fix: remove provider dependencies from up/down

--- cmd/stack.go
+++ cmd/stack.go
@@ -621,13 +621,13 @@ func init() {
 	newStackCmd.Flags().BoolVarP(&forceNewStack, "force", "f", false, "force stack creation.")
 
 	// Update Stack (Up)
-	stackCmd.AddCommand(tui.AddDependencyCheck(stackUpdateCmd, tui.Pulumi, tui.Docker, tui.DockerBuildx))
+	stackCmd.AddCommand(tui.AddDependencyCheck(stackUpdateCmd, tui.Docker, tui.DockerBuildx))
 	stackUpdateCmd.Flags().StringVarP(&envFile, "env-file", "e", "", "--env-file config/.my-env")
 	stackUpdateCmd.Flags().BoolVarP(&forceStack, "force", "f", false, "force override previous deployment")
 	tui.CheckErr(AddOptions(stackUpdateCmd, false))
 
 	// Delete Stack (Down)
-	stackCmd.AddCommand(tui.AddDependencyCheck(stackDeleteCmd, tui.Pulumi, tui.Docker))
+	stackCmd.AddCommand(tui.AddDependencyCheck(stackDeleteCmd))
 	stackDeleteCmd.Flags().BoolVarP(&confirmDown, "yes", "y", false, "confirm the destruction of the stack")
 	tui.CheckErr(AddOptions(stackDeleteCmd, false))
 

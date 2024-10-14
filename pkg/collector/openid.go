cbba16daf099e2dbaf2ae861ecdd4512971170f9
Jye Cusch <jye.cusch@nitric.io>
2024-10-14 14:31:49 +1100

    lint

--- pkg/collector/openid.go
+++ pkg/collector/openid.go
@@ -1,4 +1,12 @@
 // Copyright Nitric Pty Ltd.
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at:
+//
+// Copyright Nitric Pty Ltd.
 // 
 // SPDX-License-Identifier: Apache-2.0
 // 
@@ -13,14 +21,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package collector
-
-import (
-	"context"
-	"encoding/json"
-	"fmt"
-	"io"
-	"net/http"
 	"net/url"
 	"time"
 

eed14975107473dd6f4e533ec9e289d28cd77e02
Tim Holm <tim.holm@nitric.io>
2024-10-21 13:47:15 +1100

    fix regex

--- pkg/validation/name.go
+++ pkg/validation/name.go
@@ -30,7 +30,7 @@ var ResourceName_Rule = &Rule{
 }
 
 // create a lower kebab case only regex
-var lowerKebabCase, _ = regexp.Compile("^[a-z0-9]+(-[a-z0-9])*$")
+var lowerKebabCase, _ = regexp.Compile("^[a-z0-9]+(-[a-z0-9]+)*$")
 
 func IsValidResourceName(name string) bool {
 	return lowerKebabCase.Match([]byte(name))

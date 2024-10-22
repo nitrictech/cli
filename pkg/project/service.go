a7665031b60a1f55a737df90c5a439c36d3bb58f
Tim Holm <tim.holm@nitric.io>
2024-10-23 08:09:45 +1100

    fix: Skip DNS resolution for host.docker.internal for linux (#804)

--- pkg/project/service.go
+++ pkg/project/service.go
@@ -350,6 +350,9 @@ func (s *Service) RunContainer(stop <-chan bool, updates chan<- ServiceRunUpdate
 	if goruntime.GOOS == "linux" {
 		dockerHost := env.GetEnv("NITRIC_DOCKER_HOST", "172.17.0.1")
 
+		// FIXME: Skip dns resolution for now to allow nitric apps using deno to work on linux
+		// https://github.com/denoland/deno/issues/25927
+		runtimeOptions.nitricHost = dockerHost.String()
 		// setup host.docker.internal to route to host gateway
 		// to access rpc server hosted by local CLI run
 		hostConfig.ExtraHosts = []string{"host.docker.internal:" + dockerHost.String()}

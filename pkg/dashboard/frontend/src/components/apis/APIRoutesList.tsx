3e345b62e6d3b20a638dc2fcaacf753c2b2c7a3a
David Moore <davemooreuws@gmail.com>
2024-12-18 12:39:50 +1100

    adjust spans for api route list

--- pkg/dashboard/frontend/src/components/apis/APIRoutesList.tsx
+++ pkg/dashboard/frontend/src/components/apis/APIRoutesList.tsx
@@ -12,13 +12,13 @@ const APIRoutesList: React.FC<APIRoutesListProps> = ({
   apiAddress,
 }) => {
   return (
-    <div className="flex flex-col gap-y-2">
+    <div className="flex flex-col gap-y-2" data-testid="api-routes-list">
       {endpoints.map((endpoint) => (
         <div key={endpoint.id} className="grid w-full grid-cols-12 gap-4">
-          <div className="col-span-2 flex">
+          <div className="col-span-3 flex">
             <APIMethodBadge method={endpoint.method} />
           </div>
-          <div className="col-span-10 flex justify-start">
+          <div className="col-span-9 flex justify-start">
             <a
               target="_blank noreferrer noopener"
               className="truncate hover:underline"

4a2474f77ad8b60d3c66380725eb568df13b2008
David Moore <davemooreuws@gmail.com>
2025-01-10 15:48:07 +1100

    add more information to logs time tooltip

--- pkg/dashboard/frontend/src/components/logs/LogsExplorer.tsx
+++ pkg/dashboard/frontend/src/components/logs/LogsExplorer.tsx
@@ -5,6 +5,7 @@ import { Button } from '../ui/button'
 import { useLogs } from '@/lib/hooks/use-logs'
 import { Tooltip, TooltipContent, TooltipTrigger } from '../ui/tooltip'
 import { format } from 'date-fns/format'
+import { formatDistanceToNow } from 'date-fns/formatDistanceToNow'
 import { ansiToReact } from './ansi'
 
 const Logs: React.FC = () => {
@@ -50,14 +51,38 @@ const Logs: React.FC = () => {
                     },
                   )}
                 >
-                  <span className="w-[200px] truncate text-left">
-                    {format(new Date(time), 'MMM dd, HH:mm:ss.SS')}
-                  </span>
+                  <Tooltip>
+                    <TooltipTrigger className="w-[150px] truncate text-left">
+                      <span className="w-[200px] truncate text-left">
+                        {format(new Date(time), 'MMM dd, HH:mm:ss.SS')}
+                      </span>
+                    </TooltipTrigger>
+                    <TooltipContent
+                      className="grid grid-cols-2 gap-2 text-xs"
+                      side="right"
+                    >
+                      <span>
+                        {Intl.DateTimeFormat().resolvedOptions().timeZone}:{' '}
+                      </span>
+                      <span>
+                        {format(new Date(time), 'MMM dd, HH:mm:ss.SS')}
+                      </span>
+                      <span>Relative:</span>
+                      <span>
+                        {formatDistanceToNow(new Date(time), {
+                          addSuffix: true,
+                        })}
+                      </span>
+                      <span>Timestamp:</span>
+                      <span>{new Date(time).getTime()}</span>
+                    </TooltipContent>
+                  </Tooltip>
+
                   <Tooltip>
                     <TooltipTrigger className="w-[150px] truncate text-left">
                       <span data-testid={`test-row${i}-origin`}>{origin}</span>
                     </TooltipTrigger>
-                    <TooltipContent>
+                    <TooltipContent className="text-xs">
                       <p>{origin}</p>
                     </TooltipContent>
                   </Tooltip>

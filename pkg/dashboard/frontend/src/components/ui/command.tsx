d155e9be15b645310a2df8806047873c10869d1a
David Moore <davemooreuws@gmail.com>
2025-01-15 15:49:10 +1100

    fix lint

--- pkg/dashboard/frontend/src/components/ui/command.tsx
+++ pkg/dashboard/frontend/src/components/ui/command.tsx
@@ -37,6 +37,7 @@ const CommandInput = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive.Input>,
   React.ComponentPropsWithoutRef<typeof CommandPrimitive.Input>
 >(({ className, ...props }, ref) => (
+  // eslint-disable-next-line react/no-unknown-property -- temp fix for cmdk-input-wrapper
   <div className="flex items-center border-b px-3" cmdk-input-wrapper="">
     <Search className="mr-2 h-4 w-4 shrink-0 opacity-50" />
     <CommandPrimitive.Input
